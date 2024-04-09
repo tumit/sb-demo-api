5878ff03049784c48ccd0616797eb18db79dde77
tumit <tumit@live.com>
2024-04-10 01:03:55 +0700

    feat(roll): update slf4j

--- src/main/java/id/thedev/tumit/sbdemoapi/roll/RollController.java
+++ src/main/java/id/thedev/tumit/sbdemoapi/roll/RollController.java
@@ -2,23 +2,22 @@ package id.thedev.tumit.sbdemoapi.roll;
 
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class RollController {
-    private static final Logger logger = LoggerFactory.getLogger(RollController.class);
 
     @GetMapping("/rolldice")
     public String index(@RequestParam("player") Optional<String> player) {
         int result = this.getRandomNumber(1, 6);
         if (player.isPresent()) {
-            logger.info("{} is rolling the dice: {}", player.get(), result);
+            log.info("{} is rolling the dice: {}", player.get(), result);
         } else {
-            logger.info("Anonymous player is rolling the dice: {}", result);
+            log.info("Anonymous player is rolling the dice: {}", result);
         }
         return Integer.toString(result);
     }
